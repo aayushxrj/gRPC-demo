a6943b724fd1bc2d0594e849068ca043a520e4f1
aayushxrj <aayush.ranjan0904@gmail.com>
2025-09-11 15:13:59 +0530

    add client

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-
+# Server
 ```
 go mod init github.com/aayushxrj/gRPC-demo
 go get google.golang.org/grpc
+go mod tidy
 ```
-
 ```
 protoc --go_out=. --go-grpc_out=. proto/main.proto
 ```
 
+# Client 
+Generate code again as the client only has the main.proto file 
+
+```
+protoc --go_out=. --go-grpc_out=. proto/main.proto
+```
+```
+go mod init github.com/aayushxrj/gRPC-demo
+go get google.golang.org/grpc
+go mod tidy
+```
---
+++ client/client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	mainapipb "github.com/aayushxrj/gRPC-demo/proto/gen"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+
+	port := ":50051"
+	
+	// for without tls connections
+	conn, err := grpc.NewClient("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Println("Unable to connet", err)
+	}
+	defer conn.Close()
+
+	client := mainapipb.NewCalculateClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	req := mainapipb.AddRequest{
+		A : 10,
+		B: 20,
+	}
+
+	res, err := client.Add(ctx, &req)
+	if err != nil {
+		log.Fatalln("Could not add", err)
+	}
+
+	log.Println("Sum:", res.Sum)
+
+}
