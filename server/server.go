29517c9c0ca83e2d3d639ca3efa8326f5305b2ba
aayushxrj <aayush.ranjan0904@gmail.com>
2025-09-11 15:13:29 +0530

    add server

---
+++ server/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/aayushxrj/gRPC-demo/proto/gen"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedCalculateServer 
+}
+
+func (s *server) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddResponse, error){
+	log.Println("Add RPC called.")
+	return &pb.AddResponse{
+		Sum : req.A + req.B,
+	}, nil
+}
+
+func main (){
+
+	port := ":50051"
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("Failed to listen:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterCalculateServer(grpcServer, &server{})
+
+
+	log.Printf("Server is running on the port%s", port)
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatal("Failed to serve:", err)
+	}
+}
